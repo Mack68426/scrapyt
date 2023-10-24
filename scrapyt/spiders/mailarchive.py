3e8bf7c624165a22dda9dee74c6909375561d382
Mack <bob9531@gmail.com>
2023-10-25 00:35:49 +0800

    Add TODO an item.

--- scrapyt/spiders/mailarchive.py
+++ scrapyt/spiders/mailarchive.py
@@ -1,11 +1,9 @@
-import re
-from typing import Iterable
-import bs4
 import scrapy
+from typing import Iterable
+from bs4 import BeautifulSoup
 from scrapy.http import Request
-from scrapy.spiders import Rule, Request
+from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
-import logging
 
 class MailarchiveSpider(scrapy.Spider):
     name = "mailarchive"
@@ -14,63 +12,64 @@ class MailarchiveSpider(scrapy.Spider):
     # 選定的郵件列表名稱
     listname = 'sframe'
 
-    # 欲抓取email訊息的正規表達式
-    pattern = 'msg\/%s\/[A-Za-z0-9_-]{,27}\/' % listname
-    # start_urls = [
-    #     "https://mailarchive.ietf.org/arch/", 
-    #     "https://mailarchive.ietf.org/arch/browse/" + listname,
+    start_urls = [
+        "https://mailarchive.ietf.org/arch/browse/%s" % listname,
+    ]
 
+    # rules = [
+    #     Rule(LinkExtractor(allow=('msg\/%s\/[A-Za-z0-9_-]{,27}\/$')), callback='parse_email', follow=True)
     # ]
-
-    def start_requests(self) -> Iterable[Request]:
-        urls = [
-            "https://mailarchive.ietf.org/arch/", 
-            "https://mailarchive.ietf.org/arch/browse/%s/" % MailarchiveSpider.listname
-        ]
 
     # def _save_as_file(self, response, format='txt'):
     #     page = response.url.split('/')
     #     filename = f"page-{page}.html"
     #     file = open(f'{filename}.{format}','w+')
 
-    #     file.write(response.body)
-    
-    
+    #     file.write(response.body)``
 
     def parse(self, response):
-        for a in response.css("a.msg-detail::attr(href)").re(MailarchiveSpider.pattern):
+        self.logger.info(f'Full URL: {response.url}')
+        
+        # 欲抓取email訊息的正規表達式
+        pattern = 'msg\/%s\/[A-Za-z0-9_-]{,27}\/' % MailarchiveSpider.listname
+
+        for a in response.css("a.msg-detail::attr(href)").re(pattern):
             print('catched link:', a)
-            yield response.follow(a, self.parse_email)
-            
-                # print("Subject:", self.extract_with_css(response,"h3::text"),
-                #       "\nFrom:",    self.extract_with_css(response, "span#msg-from::text"),
-                #       "\nDate:" ,   self.extract_with_css(response, "span#msg-date::text"),
-                #       "\nContent:", self.extract_with_css(response, "pre.wordwrap::text"))
+            # print(response.url.replace(f'browse/{MailarchiveSpider.listname}/', a))
+            yield scrapy.Request(response.url.replace(f'browse/{MailarchiveSpider.listname}/', a))
         
-    # 實際解析email頁面的function
-    def parse_email(self, response):
         
+        yield from self.scrape_email(response)
+        
+    
+
+    def scrape_urls(self, response):
+        pattern = 'msg\/%s\/[A-Za-z0-9_-]{,27}\/' % MailarchiveSpider.listname
+        
+    # 抓取每個email頁面的function
+    def scrape_email(self, response):
         # 解析工具
         def extract_with_css(query):
-            return response.css(query).get(default="").strip()
+            return response.css(query).getall()
 
-        subject = extract_with_css("msg-body + h3::text")
-        fromwho = extract_with_css("span#msg-from::text")
-        date    = extract_with_css("span#msg-date::text")
-        content = extract_with_css(".wordwrap::text")
+        subjects = extract_with_css("div#msg-body h3")
+        authors  = extract_with_css("span#msg-from::text")
+        dates    = extract_with_css("span#msg-date::text")
+        contents = extract_with_css(".wordwrap::text")
 
-        print("Subject:", subject)
-        print("From   :", fromwho)
-        print("Date   :", date)
-        print("Content:", content)
+        print("Subject:", subjects)
+        print("From   :", authors)
+        print("Date   :", dates)
+        print("Content:", contents)
 
-        ScrapytItem = {
-            "mail_title": subject if subject else "NULL",
-            "mail_author":    fromwho if fromwho else "NULL",
-            "mail_date":    date if date else "NULL",
-            "mail_content": content if content else "NULL",
-        }
+
+        for (title, author, date, msg) in zip(subjects, authors, dates, contents):
+
+            ScrapytItem = {
+                "mail_title"  : title if title else None,
+                "mail_author" : author if author else None,
+                "mail_date"   : date if date else None,
+                "mail_content": msg if msg else None,
+            }
         
-        if ScrapytItem:
             yield ScrapytItem
-        
